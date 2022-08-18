16c93729ccdf102af6609804712138eef6f36266
SeongYong <pjii50260@naver.com>
2022-08-18 21:55:43 +0900

    commit

--- cloud/GCP/main.py
+++ cloud/GCP/main.py
@@ -2,19 +2,19 @@ import json
 import tweepy
 from google.cloud import pubsub_v1
 from google.oauth2 import service_account
-key_path = "ta-8755-90de8ad4bc7d.json"
+key_path = "project-tweet-359811-fb0d85feafbc.json"
 credentials = service_account.Credentials.from_service_account_file(
 key_path,
 scopes=["https://www.googleapis.com/auth/cloud-platform"],
 )
 client = pubsub_v1.PublisherClient(credentials=credentials)
-topic_path = client.topic_path('ta-8755', 'tweets')
-twitter_api_key = 'ALSKTAyOEQwSKwH6LML4u1CDb'
-twitter_api_secret_key = 'gdA2jRr6VCpZpYAqOB6t94xp2NFjUHj5EFp4l8vNI1rFgcwPgs'
-twitter_access_token = '1558754145355476992-PYGHMZxkVpuRi3OsMJroNxwfDx3b4c'
-twitter_access_token_secret = 'qBY5CaZbwnviN161VnmT3ohewKSL7f3IPSnwlYJjO1tTk'
+topic_path = client.topic_path('project-tweet', 'tweets')
+consumer_key = 'ALSKTAyOEQwSKwH6LML4u1CDb'
+consumer_secret = 'gdA2jRr6VCpZpYAqOB6t94xp2NFjUHj5EFp4l8vNI1rFgcwPgs'
+access_token  = '1558754145355476992-PYGHMZxkVpuRi3OsMJroNxwfDx3b4c'
+access_token_secret  = 'qBY5CaZbwnviN161VnmT3ohewKSL7f3IPSnwlYJjO1tTk'
 
-class SimpleStreamListener(tweepy.StreamListener):
+class SimpleStreamListener(tweepy.Client):
     def on_status(self, status):
         print(status)
         tweet = json.dumps({'id': status.id, 'created_at': status.created_at, 'text': status.text}, default=str)
@@ -24,8 +24,7 @@ class SimpleStreamListener(tweepy.StreamListener):
         if status_code == 420:
             return False
 
-stream_listener = SimpleStreamListener()
-auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret_key)
-auth.set_access_token(twitter_access_token, twitter_access_token_secret)
-twitterStream = tweepy.Stream(auth, stream_listener)
-twitterStream.filter(track=['data'])
+stream_listener = SimpleStreamListener(consumer_key, consumer_secret,access_token, access_token_secret)
+
+stream_listener.get_bookmarks()
+
